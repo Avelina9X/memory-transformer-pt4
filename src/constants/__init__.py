46ee2e7619d89887386671b445ed83ff00770304
Avelina Kyriacou <lhk3@st-andrews.ac.uk>
2023-12-03 13:39:27 +0000

    pylint whitespace compliance

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -24,11 +24,11 @@ TORCH_COMPILE_OPTIONS = {
         'triton.autotune_pointwise': True,
         'triton.autotune_cublasLt': True,
         'shape_padding': True,
-        
+
         'max_autotune': True,
         'max_autotune_pointwise': True,
         'max_autotune_gemm': True,
-        
+
         'allow_buffer_reuse': True,
         'epilogue_fusion': True,
     },
