d32820bcd43b735bd69508d56d63eb12de0df109
Avelina Kyriacou <lhk3@st-andrews.ac.uk>
2024-03-09 22:56:56 +0000

    added docstrings to formatter

--- src/training/data_instruct/formatter.py
+++ src/training/data_instruct/formatter.py
@@ -12,7 +12,7 @@ class InstructionFormatter():
         assert isinstance( output, list )
         return output
     
-    def apply_chat_template_line( self, message: Message ):
+    def _apply_chat_template_line( self, message: Message ):
         role = message.role
         content = message.content
         complete = message.complete
@@ -39,8 +39,8 @@ class InstructionFormatter():
             'test_mask': prefix_test_mask + content_test_mask + suffix_test_mask,
         }
     
-    def apply_chat_template( self, conversation: MessageList ):
-        lines = [ self.apply_chat_template_line( line ) for line in conversation ]
+    def _apply_chat_template( self, conversation: MessageList ):
+        lines = [ self._apply_chat_template_line( line ) for line in conversation ]
         
         tokens = [ line[ 'tokens' ] for line in lines ]
         train_mask = [ line[ 'train_mask' ] for line in lines ]
@@ -52,10 +52,20 @@ class InstructionFormatter():
             'test_mask': list( itertools.chain( *test_mask ) ),
         }
     
-    def tokenize_chat( self, conversation: MessageList ):
-        # TODO: assert final message is complete
+    def _remove_system_msgs( self, msgs: MessageList ):
+        return [ msg for msg in msgs if msg.role != 'system' ]
+    
+    def tokenize_chat( self, conversation: MessageList ) -> dict:
+        """ Tokenizes a message list for zero-shot evaluation.
 
-        outputs = self.apply_chat_template( conversation )
+        Args:
+            conversation (MessageList): Target message to evaluate.
+
+        Returns:
+            dict: dictionary of tokens and masks
+        """
+
+        outputs = self._apply_chat_template( conversation )
         
         return {
             'tokens': [ self.tokenizer.eos_token_id ] + outputs[ 'tokens' ],
@@ -64,15 +74,24 @@ class InstructionFormatter():
             'test_mask': outputs[ 'test_mask' ] + [ False ],
         }
     
-    def _remove_system_msgs( self, msgs: MessageList ):
-        return [ msg for msg in msgs if msg.role != 'system' ]
-    
     def tokenize_chat_fewshot(
         self,
         target_conversation: MessageList,
         fewshot_list: list[MessageList],
         fewshow_allsys: bool
-    ):
+    ) -> dict:
+        """ Tokenizes a message list for few-shot evaluation.
+
+        Only the final assistant message is enabled in the mask.
+
+        Args:
+            target_conversation (MessageList): Target message to evaluate.
+            fewshot_list (list[MessageList]): Example few shot messages.
+            fewshow_allsys (bool): When True all messages have a system message. When False only the first system message is included.
+
+        Returns:
+            dict: dictionary of tokens and masks
+        """
         head = fewshot_list[0]
         body = list( itertools.chain( *fewshot_list[ 1 : ] ) )
         tail = target_conversation
@@ -81,8 +100,8 @@ class InstructionFormatter():
             body = self._remove_system_msgs( body )
             tail = self._remove_system_msgs( tail )
         
-        f_outputs = self.apply_chat_template( head + body )
-        t_outputs = self.apply_chat_template( tail )
+        f_outputs = self._apply_chat_template( head + body )
+        t_outputs = self._apply_chat_template( tail )
 
         f_outputs[ 'train_mask' ] = [ False for _ in f_outputs[ 'train_mask' ] ]
         f_outputs[ 'test_mask' ] = [ False for _ in f_outputs[ 'test_mask' ] ]
@@ -93,16 +112,45 @@ class InstructionFormatter():
             'train_mask': f_outputs[ 'train_mask' ] + t_outputs[ 'train_mask' ] + [ False ],
             'test_mask': f_outputs[ 'test_mask' ] + t_outputs[ 'test_mask' ] + [ False ],
         }
-
     
-    def tokenize_generation( self, conversation: MessageList ):
-        # TODO: assert final message is incomplete
+    def tokenize_chat_training(
+        self,
+        target_list: list[MessageList],
+        fewshow_allsys: bool
+    ) -> dict:
+        """ Tokenizes a message list for zero-shot or few-shot.
 
-        outputs = self.apply_chat_template( conversation )
+        All assistant message are enabled in the mask.
+
+        Args:
+            target_list (list[MessageList]): List of target messages.
+            fewshow_allsys (bool): When True all messages have a system message. When False only the first system message is included.
+
+        Returns:
+            dict: dictionary of tokens and masks
+        """
+        head = target_list[0]
+        body = list( itertools.chain( *target_list[ 1 : ] ) )
+
+        if not fewshow_allsys:
+            body = self._remove_system_msgs( body )
         
+        outputs = self._apply_chat_template( head + body )
+
         return {
             'tokens': [ self.tokenizer.eos_token_id ] + outputs[ 'tokens' ],
-            'targets': None,
-            'train_mask': None,
-            'test_mask': None,
+            'targets': outputs[ 'tokens' ] + [ self.tokenizer.eos_token_id ],
+            'train_mask': outputs[ 'train_mask' ] + [ False ],
+            'test_mask': outputs[ 'test_mask' ] + [ False ],
         }
+
+    
+    # def tokenize_generation( self, conversation: MessageList ):
+    #     # TODO: assert final message is incomplete
+    #     outputs = self._apply_chat_template( conversation )
+    #     return {
+    #         'tokens': [ self.tokenizer.eos_token_id ] + outputs[ 'tokens' ],
+    #         'targets': None,
+    #         'train_mask': None,
+    #         'test_mask': None,
+    #     }
