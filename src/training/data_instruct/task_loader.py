1552085602c8800090b82dc99ae513b45716e85b
Avelina Kyriacou <lhk3@st-andrews.ac.uk>
2024-03-11 16:26:52 +0000

    added multitask loading

--- src/training/data_instruct/task_loader.py
+++ src/training/data_instruct/task_loader.py
@@ -1,11 +1,12 @@
 from collections.abc import Iterable, Generator
 import gc
 import random
+from typing import TypeAlias
 
 import torch
 from torch.utils.data import IterableDataset, DataLoader
 
-from .task_base import BaseInstructDataset
+from .task_base import BaseInstructDataset, MessageList
 from .formatter import InstructionFormatter
 
 
@@ -25,8 +26,9 @@ class TaskLoader( IterableDataset ):
         fewshot_allsys: bool,
         seq_length: int,
         sub_batch_size: int,
-        shuffle_seed: int | None,
+        shuffle_seed: int | bool,
         mask_type: str,
+        shard_mode: str = 'shuffle',
     ):
         """ Creates a task loader for a single task.
 
@@ -37,13 +39,15 @@ class TaskLoader( IterableDataset ):
             fewshot_allsys (bool): If all message groups should contain a system message
             seq_length (int): Max tokens per sequence in batch
             sub_batch_size (int): Number of sequences per batch
-            shuffle_seed (int | None): Dataset shuffling seed, or None for shuffling disabled
+            shuffle_seed (int | bool): Dataset pre-shuffling seed, or True for random seed, or False for no shuffle.
             mask_type (str): Must be 'all', 'train' or 'test'
+            shard_mode (str): 'split' uses exclusive sharding, 'shuffle' shards by shuffling. Defaults to 'shuffle'.
 
         Raises:
             ValueError: If task does not have training docs
             ValueError: If `fewshow_count` is less than 1
             ValueError: If `mask_type` is not 'all', 'train' or 'test'
+            ValueError: If `shard_mode` is not 'split' or 'shuffle'
         """
 
         if not task.has_training_docs:
@@ -55,6 +59,9 @@ class TaskLoader( IterableDataset ):
         if mask_type not in [ 'all', 'train', 'test' ]:
             raise ValueError( 'mask_type must be `all`, `train` or `test`' )
 
+        if shard_mode not in [ 'split', 'shuffle' ]:
+            raise ValueError( 'shard_mode must be `split` or `shuffle`' )
+
         self.task = task
         self.formatter = formatter
         self.fewshot_count = fewshot_count
@@ -62,27 +69,37 @@ class TaskLoader( IterableDataset ):
         self.seq_length = seq_length
         self.sub_batch_size = sub_batch_size
         self.mask_type = mask_type
+        self.shard_mode = shard_mode
 
         dataset = self.task.get_training_docs()
         assert dataset is not None
 
-        if shuffle_seed is not None:
+        if shuffle_seed is True:
+            dataset = dataset.shuffle( None, keep_in_memory=True )
+        elif shuffle_seed is not False:
             dataset = dataset.shuffle( shuffle_seed, keep_in_memory=True )
 
-        self._dataset_shards = [
-            dataset.shard( sub_batch_size, i, keep_in_memory=True )
-            for i in range( sub_batch_size )
-        ]
+        match shard_mode:
+            case 'split':
+                self._dataset_shards = [
+                    dataset.shard( sub_batch_size, i, keep_in_memory=True )
+                    for i in range( sub_batch_size )
+                ]
+            case 'shuffle':
+                self._dataset_shards = [
+                    dataset.shuffle( i, keep_in_memory=True )
+                    for i in range( sub_batch_size )
+                ]
 
-    def messages_generator( self, shard_idx: int ):
-        iterator = iter( generate_forever( self._dataset_shards[shard_idx] ) )
-
-        while True:
-            message_list = [
-                random.choice( self.task.create_target_message_list( next( iterator ) ) )
+    def message_list_generator( self, shard_idx: int ) -> Generator[list[MessageList], None, None]:
+        for elem in generate_forever( self._dataset_shards[shard_idx] ):
+            yield [
+                random.choice( self.task.create_target_message_list( elem ) )
                 for _ in range( self.fewshot_count )
             ]
 
+    def message_tokens_generator( self, shard_idx: int ) -> Generator[tuple[list[int], list[int]], None, None]:
+        for message_list in self.message_list_generator( shard_idx ):
             token_dict = self.formatter.tokenize_chat_training( message_list, self.fewshot_allsys )
 
             tokens = token_dict[ 'tokens' ]
@@ -93,33 +110,25 @@ class TaskLoader( IterableDataset ):
             match self.mask_type:
                 case 'all':
                     yield tokens, targets
-
                 case 'train':
                     yield tokens, [ t if m else -100 for t, m in zip( targets, train_mask ) ]
-
                 case 'test':
                     yield tokens, [ t if m else -100 for t, m in zip( targets, test_mask ) ]
 
-    def token_generator( self, shard_idx: int ):
-        for messages in self.messages_generator( shard_idx ):
+    def token_generator( self, shard_idx: int ) -> Generator[tuple[int, int], None, None]:
+        for messages in self.message_tokens_generator( shard_idx ):
             for x, y in zip( messages[0], messages[1] ):
                 yield x, y
 
-    def sub_batch_generator( self ):
+    def sub_batch_generator( self ) -> Generator[tuple[torch.Tensor, torch.Tensor], None, None]:
         def reset():
-            count = 0
-            xs = []
-            ys = []
+            count, xs, ys = 0, [], []
             for _ in range( self.sub_batch_size ):
                 xs.append( [] )
                 ys.append( [] )
-
             return count, xs, ys
-
         count, xs, ys = reset()
-
         generators = [ iter( self.token_generator( i ) ) for i in range( self.sub_batch_size ) ]
-
         try:
             while True:
                 for g_idx, generator in enumerate( generators ):
@@ -130,7 +139,6 @@ class TaskLoader( IterableDataset ):
 
                 if count == self.seq_length:
                     yield ( torch.LongTensor( xs ), torch.LongTensor( ys ) )
-
                     count, xs, ys = reset()
         except StopIteration:
             return
@@ -149,14 +157,16 @@ class TaskLoader( IterableDataset ):
     def __getitem__( self, index ):
         raise NotImplementedError( "This dataset does not support random access using __getitem__" )
 
+TaskList: TypeAlias = list[tuple[BaseInstructDataset, int, bool]]
+
 class MultiTaskLoader( IterableDataset ):
     def __init__(
         self,
-        task_list: list[tuple[BaseInstructDataset, int, bool]],
+        task_list: TaskList,
         formatter: InstructionFormatter,
         seq_length: int,
         batch_size: int,
-        shuffle_seed: int | None,
+        shuffle_seed: int | bool,
         mask_type: str,
     ):
         sub_batch_size = batch_size // len( task_list )
@@ -199,6 +209,88 @@ class MultiTaskLoader( IterableDataset ):
             self,
             num_workers=0,
             batch_size=None,
+            pin_memory=True,
+            pin_memory_device='cuda',
+        )
+
+    def __getitem__( self, index ):
+        raise NotImplementedError( "This dataset does not support random access using __getitem__" )
+
+class MixedTaskLoader( IterableDataset ):
+    def __init__(
+        self,
+        task_list: TaskList,
+        formatter: InstructionFormatter,
+        seq_length: int,
+        batch_size: int,
+        shuffle_seed: int | bool,
+        mask_type: str,
+    ):
+        self.batch_size = batch_size
+        self.seq_length = seq_length
+
+        self.tasks = [
+            TaskLoader(
+                task=task,
+                formatter=formatter,
+                fewshot_count=fewshot_count,
+                fewshot_allsys=fewshot_allsys,
+                seq_length=seq_length,
+                sub_batch_size=batch_size,
+                shuffle_seed=shuffle_seed,
+                mask_type=mask_type
+            )
+            for task, fewshot_count, fewshot_allsys
+            in task_list
+        ]
+
+    def message_tokens_generator( self, shard_idx: int ) -> Generator[tuple[list[int], list[int]], None, None]:
+        generators = [
+            iter( task.message_tokens_generator( shard_idx ) )
+            for task in self.tasks
+        ]
+
+        while True:
+            for gen in generators:
+                yield next( gen )
+
+    def token_generator( self, shard_idx: int ) -> Generator[tuple[int, int], None, None]:
+        for messages in self.message_tokens_generator( shard_idx ):
+            for x, y in zip( messages[0], messages[1] ):
+                yield x, y
+
+    def sub_batch_generator( self ) -> Generator[tuple[torch.Tensor, torch.Tensor], None, None]:
+        def reset():
+            count, xs, ys = 0, [], []
+            for _ in range( self.batch_size ):
+                xs.append( [] )
+                ys.append( [] )
+            return count, xs, ys
+        count, xs, ys = reset()
+        generators = [ iter( self.token_generator( i ) ) for i in range( self.batch_size ) ]
+        try:
+            while True:
+                for g_idx, generator in enumerate( generators ):
+                    x, y = next( generator )
+                    xs[ g_idx ].append( x )
+                    ys[ g_idx ].append( y )
+                count += 1
+
+                if count == self.seq_length:
+                    yield ( torch.LongTensor( xs ), torch.LongTensor( ys ) )
+                    count, xs, ys = reset()
+        except StopIteration:
+            return
+
+    def __iter__( self ):
+        return iter( self.sub_batch_generator() )
+
+    def as_data_loader( self ):
+        return DataLoader(
+            self,
+            num_workers=1,
+            batch_size=None,
+            prefetch_factor=4,
             pin_memory=True,
             pin_memory_device='cuda',
         )
