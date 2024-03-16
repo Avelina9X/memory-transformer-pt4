41a43b3aed5c995c7eda5774015be97d24ffe13a
Avelina Kyriacou <lhk3@st-andrews.ac.uk>
2024-03-16 16:20:50 +0000

    added squad and squad_v2

---
+++ src/training/data_instruct/tasks/squad.py
@@ -0,0 +1,208 @@
+from datasets import DatasetDict, Dataset, load_dataset
+from evaluate import load as load_metric
+import spacy
+
+from ..task_base import BaseInstructDataset, InstructionDatasetTask, Message
+
+class SquadV1InstructDataset( BaseInstructDataset ):
+    def __init__( self, cache_dir: str ):
+        self.metric = load_metric( 'squad' )
+        super().__init__( cache_dir )
+
+    def download( self, cache_dir: str ) -> DatasetDict:
+        dataset = load_dataset( 'rajpurkar/squad', cache_dir=cache_dir )
+        assert isinstance( dataset, DatasetDict )
+        return dataset
+
+    @property
+    def task_type( self ) -> InstructionDatasetTask:
+        return InstructionDatasetTask.SPAN_EXTRACTION
+
+    @property
+    def task_name( self ) -> str:
+        return 'squad'
+
+    @property
+    def task_subset( self ) -> str:
+        return 'v1.1'
+
+    def get_training_docs( self ) -> Dataset:
+        return self.dataset[ 'train' ]
+
+    def get_validation_docs( self ) -> Dataset:
+        return self.dataset[ 'validation' ]
+
+    def get_test_docs( self ) -> None:
+        return None
+
+    def get_fewshot_docs( self ) -> None:
+        return None
+
+    def format_user_message( self, doc: dict ) -> Message:
+        prompt = (
+            f'Title: {doc["title"]}\n'
+            f'\n'
+            f'Background: {doc["context"]}\n'
+            f'\n'
+            f'Question: {doc["question"]}\n'
+            f'\n'
+            f'Answer:'
+        )
+
+        return Message(
+            role='user',
+            content=prompt,
+            complete=True,
+        )
+
+    def format_target_messages( self, doc: dict ) -> list[Message]:
+        return [
+            Message(
+                role='assistant',
+                content=answer,
+                complete=True,
+            )
+            for answer in doc[ 'answers' ][ 'text' ]
+        ]
+
+    def format_distractor_messages( self, doc: dict ) -> list[Message]:
+        return []
+
+    def format_unlabelled_messages( self, doc: dict ) -> list[Message]:
+        return []
+
+    def create_unlabelled_message_target( self, doc: dict ) -> None:
+        return None
+
+    def compute_metric( self, predictions=None, references=None ) -> dict:
+        metric = self.metric.compute( predictions=predictions, references=references )
+        assert metric is not None
+        return metric
+
+
+class SquadV2InstructDataset( BaseInstructDataset ):
+    def __init__( self, cache_dir: str ):
+        self.metric = load_metric( 'squad_v2' )
+        self._nlp = spacy.load( 'en_core_web_sm' )
+        super().__init__( cache_dir )
+
+    def download( self, cache_dir: str ) -> DatasetDict:
+        dataset = load_dataset( 'rajpurkar/squad_v2', cache_dir=cache_dir )
+        assert isinstance( dataset, DatasetDict )
+        return dataset
+
+    @property
+    def task_type( self ) -> InstructionDatasetTask:
+        return InstructionDatasetTask.SPAN_EXTRACTION_V2
+
+    @property
+    def task_name( self ) -> str:
+        return 'squad'
+
+    @property
+    def task_subset( self ) -> str:
+        return 'v2'
+
+    def get_training_docs( self ) -> Dataset:
+        return self.dataset[ 'train' ]
+
+    def get_validation_docs( self ) -> Dataset:
+        return self.dataset[ 'validation' ]
+
+    def get_test_docs( self ) -> None:
+        return None
+
+    def get_fewshot_docs( self ) -> None:
+        return None
+
+    def format_user_message( self, doc: dict ) -> Message:
+        prompt = (
+            f'Title: {doc["title"]}\n'
+            f'\n'
+            f'Background: {doc["context"]}\n'
+            f'\n'
+            f'Question: {doc["question"]}\n'
+            f'\n'
+            f'Answer:'
+        )
+
+        return Message(
+            role='user',
+            content=prompt,
+            complete=True,
+        )
+
+    def format_target_messages( self, doc: dict ) -> list[Message]:
+        if len( doc[ 'answers' ][ 'text' ] ) > 0:
+            return [
+                Message(
+                    role='assistant',
+                    content=answer,
+                    complete=True,
+                )
+                for answer in doc[ 'answers' ][ 'text' ]
+            ]
+        else:
+            return [
+                Message(
+                    role='assistant',
+                    content='unanswerable',
+                    complete=True,
+                )
+            ]
+
+    def format_distractor_messages( self, doc: dict ) -> list[Message]:
+        if len( doc[ 'answers' ][ 'text' ] ) > 0:
+            return [
+                Message(
+                    role='assistant',
+                    content='unanswerable',
+                    complete=True,
+                )
+            ]
+        else:
+            document = self._nlp( doc["context"] )
+            return [
+                Message(
+                    role='assistant',
+                    content=chunk.text,
+                    complete=True,
+                )
+                for chunk in document.noun_chunks
+            ]
+
+    def format_unlabelled_messages( self, doc: dict ) -> list[Message]:
+        return []
+
+    def create_unlabelled_message_target( self, doc: dict ) -> None:
+        return None
+
+    def compute_metric( self, predictions=None, references=None ) -> dict:
+        metric = self.metric.compute( predictions=predictions, references=references )
+        assert metric is not None
+        return metric
+
+
+def main():
+    # pylint: disable=W0611
+    # pylint: disable=C0415
+    import os
+    import rich
+
+    cache_dir = os.environ[ 'HF_CACHE_DIR' ]
+
+    rich.print( SquadV1InstructDataset( cache_dir ) )
+    rich.print( SquadV2InstructDataset( cache_dir ) )
+
+    squad_v2 = SquadV2InstructDataset( cache_dir )
+
+    dataset = squad_v2.get_training_docs()
+
+    print( squad_v2.format_user_message( dataset[0] ) )
+    print( squad_v2.format_target_messages( dataset[0] ) )
+    print( squad_v2.format_distractor_messages( dataset[0] ) )
+    print()
+    print( squad_v2.format_user_message( dataset[-1] ) )
+    print( squad_v2.format_target_messages( dataset[-1] ) )
+    print( squad_v2.format_distractor_messages( dataset[-1] ) )
+    print()
