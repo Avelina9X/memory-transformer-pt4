75c6cf0a702ae30ac1e8752dadf8439319a2cb04
Avelina Kyriacou <lhk3@st-andrews.ac.uk>
2024-03-18 22:54:19 +0000

    updated formatter to only include first suffix token

--- src/training/data_instruct/formatter.py
+++ src/training/data_instruct/formatter.py
@@ -6,55 +6,55 @@ from .task_base import Message, MessageList
 class InstructionFormatter():
     def __init__( self, tokenizer: PreTrainedTokenizerBase ):
         self.tokenizer = tokenizer
-    
+
     def _tokenize( self, text: str ) -> list[int]:
         output = self.tokenizer( text, add_special_tokens=False )[ 'input_ids' ]
         assert isinstance( output, list )
         return output
-    
+
     def _apply_chat_template_line( self, message: Message ):
         role = message.role
         content = message.content
         complete = message.complete
-        
+
         # TODO: assertions for role and completion
-        
+
         trainable: bool = role == 'assistant'
-        
+
         prefix = self._tokenize( f'<|im_start|>{role}\n' )
         content = self._tokenize( content )
         suffix = self._tokenize( '<|im_end|>\n' if complete else '' )
-        
+
         prefix_train_mask = [ False for _ in prefix ]
         content_train_mask = [ trainable for _ in content ]
-        suffix_train_mask = [ trainable for _ in suffix ]
-        
+        suffix_train_mask = [ trainable and i == 0 for i, _ in enumerate( suffix ) ]
+
         prefix_test_mask = [ False for _ in prefix ]
         content_test_mask = [ trainable for _ in content ]
         suffix_test_mask = [ False for _ in suffix ]
-        
+
         return {
             'tokens': prefix + content + suffix,
             'train_mask': prefix_train_mask + content_train_mask + suffix_train_mask,
             'test_mask': prefix_test_mask + content_test_mask + suffix_test_mask,
         }
-    
+
     def _apply_chat_template( self, conversation: MessageList ):
         lines = [ self._apply_chat_template_line( line ) for line in conversation ]
-        
+
         tokens = [ line[ 'tokens' ] for line in lines ]
         train_mask = [ line[ 'train_mask' ] for line in lines ]
         test_mask = [ line[ 'test_mask' ] for line in lines ]
-        
+
         return {
             'tokens': list( itertools.chain( *tokens ) ),
             'train_mask': list( itertools.chain( *train_mask ) ),
             'test_mask': list( itertools.chain( *test_mask ) ),
         }
-    
+
     def _remove_system_msgs( self, msgs: MessageList ):
         return [ msg for msg in msgs if msg.role != 'system' ]
-    
+
     def tokenize_chat( self, conversation: MessageList ) -> dict:
         """ Tokenizes a message list for zero-shot evaluation.
 
@@ -66,14 +66,14 @@ class InstructionFormatter():
         """
 
         outputs = self._apply_chat_template( conversation )
-        
+
         return {
             'tokens': [ self.tokenizer.eos_token_id ] + outputs[ 'tokens' ],
             'targets': outputs[ 'tokens' ] + [ self.tokenizer.eos_token_id ],
             'train_mask': outputs[ 'train_mask' ] + [ False ],
             'test_mask': outputs[ 'test_mask' ] + [ False ],
         }
-    
+
     def tokenize_chat_fewshot(
         self,
         target_conversation: MessageList,
@@ -99,7 +99,7 @@ class InstructionFormatter():
         if not fewshow_allsys:
             body = self._remove_system_msgs( body )
             tail = self._remove_system_msgs( tail )
-        
+
         f_outputs = self._apply_chat_template( head + body )
         t_outputs = self._apply_chat_template( tail )
 
@@ -112,7 +112,7 @@ class InstructionFormatter():
             'train_mask': f_outputs[ 'train_mask' ] + t_outputs[ 'train_mask' ] + [ False ],
             'test_mask': f_outputs[ 'test_mask' ] + t_outputs[ 'test_mask' ] + [ False ],
         }
-    
+
     def tokenize_chat_training(
         self,
         target_list: list[MessageList],
@@ -134,7 +134,7 @@ class InstructionFormatter():
 
         if not fewshow_allsys:
             body = self._remove_system_msgs( body )
-        
+
         outputs = self._apply_chat_template( head + body )
 
         return {
@@ -144,7 +144,7 @@ class InstructionFormatter():
             'test_mask': outputs[ 'test_mask' ] + [ False ],
         }
 
-    
+
     # def tokenize_generation( self, conversation: MessageList ):
     #     # TODO: assert final message is incomplete
     #     outputs = self._apply_chat_template( conversation )
