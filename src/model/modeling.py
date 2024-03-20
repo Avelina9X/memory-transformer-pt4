259a44913dae6d960d225ae8a05deefd10526416
Avelina Kyriacou <lhk3@st-andrews.ac.uk>
2024-03-20 18:08:39 +0000

    updated model with correct cache reordering

--- src/model/modeling.py
+++ src/model/modeling.py
@@ -373,12 +373,10 @@ class LSWTForCausalLM( LSWTPreTrainedModel ):
         )
         return model_inputs
 
-    @staticmethod
-    def _reorder_cache(past_key_values, beam_idx):
+    def _reorder_cache( self, past_key_values, beam_idx ):
         reordered_past = ()
         for layer_past in past_key_values:
-            new_val = tuple(past_state.index_select(0, beam_idx.to(past_state.device)) for past_state in layer_past)
             reordered_past += (
-                new_val,
+                tuple(past_state.index_select(0, beam_idx.to(past_state.device)) for past_state in layer_past),
             )
         return reordered_past
