b2eea8280d78203e12fee2a1a7e0161a45574159
Avelina Kyriacou <lhk3@st-andrews.ac.uk>
2024-04-13 16:23:59 +0100

    added docstrings and glue zipping

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,8 +1,9 @@
-
+""" Module for benchmarking models. """
 
 import os
 import typing
 import argparse
+import shutil
 
 import torch
 import tqdm
@@ -17,13 +18,17 @@ from training.data_instruct.tasks import DIRECTORY_CHOICE
 
 from constants import HF_CACHE_DIR
 
+
 def evaluate_glue(
     batcher: ChoiceInstructionBatcher | DPHChoiceInstructionBatcher,
     eval_dir: str,
     is_dph: bool
 ):
-    """ Evaluates on the GLUE dataset. Honestly this can probably be done a LOT better.
+    """ Evaluates on the GLUE dataset.
+
+    Honestly this can probably be done a LOT better.
     GLUE is just a little weird since we need to upload to a test server.
+    TODO: auto zip the files?
 
     Args:
         batcher (ChoiceInstructionBatcher | DPHChoiceInstructionBatcher): The batcher to be used for eval.
@@ -38,10 +43,10 @@ def evaluate_glue(
     eval_dir_log = os.path.join( eval_dir, 'glue_log' )
     eval_dir_dph = os.path.join( eval_dir, 'glue_dph' ) if is_dph else None
 
+    # Create directories
     os.makedirs( eval_dir_log, mode=0o777, exist_ok=True )
     if eval_dir_dph:
         os.makedirs( eval_dir_dph, mode=0o777, exist_ok=True )
-
 
     # Create a map of all tasks
     tasks_map = [
@@ -71,7 +76,7 @@ def evaluate_glue(
         # Get the features to decide if we're a regression or classifcation task
         label_features = test_ds.features[ 'label' ]
 
-        # Create the label2id lambda TODO: don't do this. there's a better way
+        # Create the label2id lambdas TODO: don't do this. there's a better way
         if hasattr( label_features, 'names' ):
             label2id = label_features.names.__getitem__
         elif hasattr( label_features, 'dtype' ):
@@ -79,29 +84,55 @@ def evaluate_glue(
         else:
             raise ValueError( 'Somehow the features are neither multiple choice nor floating point' )
 
+        # Create empty list for all results
         results_list = []
 
+        # Iterate through dataset and get predictions
         for line in test_ds:
             result = batcher.evaluate_document( task, line, False, False )
             result[ 'id' ] = line[ 'idx' ]
             results_list.append( result )
 
+        # If we don't have dph write only to the log dir
         if task_dir_dph is None:
             with open( task_dir_log, 'w', encoding='utf-8' ) as f:
+                # Headers
                 f.write( 'index\tprediction\n' )
 
+                # Predictions
                 for result in results_list:
                     f.write( f'{result["id"]}\t{label2id(result["predictions"].item())}\n' )
+
+        # If we do have dph, write to both dph and log dirs
         else:
             with open( task_dir_log, 'w', encoding='utf-8' ) as f_log, open( task_dir_dph, 'w', encoding='utf-8' ) as f_dph:
+                # Headers
                 f_log.write( 'index\tprediction\n' )
                 f_dph.write( 'index\tprediction\n' )
 
+                # Predictions
                 for result in results_list:
                     f_log.write( f'{result["id"]}\t{label2id(result["log_predictions"].item())}\n' )
                     f_dph.write( f'{result["id"]}\t{label2id(result["dph_predictions"].item())}\n' )
 
-def weighted_score( metrics: dict[str, float], weights: dict[str, float] ):
+    # Zip log prob predictions
+    shutil.make_archive( os.path.join( eval_dir_log, 'submission' ), 'zip', eval_dir_log )
+
+    # Zip dph predictions if they exist
+    if eval_dir_dph:
+        shutil.make_archive( os.path.join( eval_dir_dph, 'submission' ), 'zip', eval_dir_dph )
+
+
+def weighted_score( metrics: dict[str, float], weights: dict[str, float] ) -> float:
+    """ Computes the weighted average score of all sub tasks.
+
+    Args:
+        metrics (dict[str, float]): Metric dict of task:accuracy
+        weights (dict[str, float]): DIct of task:weight
+
+    Returns:
+        float: Weighted average score
+    """
     accumulated_acc = 0.0
     accumulated_weight = 0.0
 
@@ -120,7 +151,9 @@ def evaluate_generic(
     task_map: list[tuple[str, bool, BaseChoiceInstructDataset, float]],
 ):
     """ Evaluates on an arbitrary weighted task list.
+
     For unweighted aggregations simply pass 1.0 for all task weights.
+    Otherwise pass the sample count for a weighted average.
 
     Args:
         batcher (ChoiceInstructionBatcher | DPHChoiceInstructionBatcher): The batcher to be used for eval.
@@ -149,6 +182,7 @@ def evaluate_generic(
         # Compute the results
         results = batcher.evaluate_dataset( task, test_ds, False, False )
 
+        # Add the weighting
         metrics_weights[ task_name ] = weight
 
         # Add log or log and dph results to metric dicts
@@ -177,11 +211,24 @@ def evaluate_generic(
             metrics_dph_tsv = '\t'.join( [ str( i ) for i in metrics_dph.values() ] )
             f.write( f'dph\t{metrics_dph_tsv}\t{score_dph}\n')
 
+
 def evaluate_gpt4all(
     batcher: ChoiceInstructionBatcher | DPHChoiceInstructionBatcher,
     eval_dir: str,
     is_dph: bool
 ):
+    """ Evaluates on the GPT4All tasks.
+
+    Average score is unweighted macro average of accuracies.
+    Following LM Evaluation Harness only `OpenBookQA` and `ARC` use the test splits,
+    while all other tasks use the validation splits.
+
+    Args:
+        batcher (ChoiceInstructionBatcher | DPHChoiceInstructionBatcher): The batcher to be used for eval.
+        eval_dir (str): Directory to save results to (we'll create a tsv file in here).
+        is_dph (bool): If we're doing DPH or not. We can figure this out from the batcher class, but lets just be sure.
+    """
+
     # Create a map of all tasts
     task_map: list[tuple[str, bool, BaseChoiceInstructDataset, float]] = [
         ( 'HellaSwag', False, DIRECTORY_CHOICE[ 'hellaswag' ][ 'no_choice' ]( HF_CACHE_DIR ), 1.0 ),
@@ -200,6 +247,16 @@ def evaluate_race(
     eval_dir: str,
     is_dph: bool
 ):
+    """ Evaluates RACE middle and high on the test splits.
+
+    Computes the average score as the weighted average of both subsets (m=1436, h=3498)
+
+    Args:
+        batcher (ChoiceInstructionBatcher | DPHChoiceInstructionBatcher): The batcher to be used for eval.
+        eval_dir (str): Directory to save results to (we'll create a tsv file in here)
+        is_dph (bool): If we're doing DPH or not. We can figure this out from the batcher class, but lets just be sure.
+    """
+
     # Create a map of all tasts
     task_map: list[tuple[str, bool, BaseChoiceInstructDataset, float]] = [
         ( 'RACE-m', True, DIRECTORY_CHOICE[ 'race' ][ 'middle' ]( HF_CACHE_DIR ), 1436 ),
@@ -208,7 +265,14 @@ def evaluate_race(
 
     evaluate_generic( batcher, eval_dir, is_dph, 'race.tsv', task_map )
 
+
 def evaluate( model_dir: str, benchmark: str ):
+    """ Evaluates a model using a specific benchmark
+
+    Args:
+        model_dir (str): directory containing model weights, config and tokenizer
+        benchmark (str): string identifier for benchmark
+    """
 
     # Create directory inside model_dir with evaluation results
     eval_dir = os.path.join( model_dir, 'benchmarks' )
