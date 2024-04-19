7571d90245f62864f995c8d583c9ae59a2fccd7a
Avelina Kyriacou <lhk3@st-andrews.ac.uk>
2024-04-19 18:15:58 +0100

    added copa to superglue

--- src/training/data_instruct/tasks/super_glue.py
+++ src/training/data_instruct/tasks/super_glue.py
@@ -4,11 +4,10 @@ from evaluate import load as load_metric
 
 from ..task_base import BaseChoiceInstructDataset, InstructionDatasetTask, Message
 
-class BoolQInstructDataset( BaseChoiceInstructDataset ):
+class BoolQInstructDataset( BaseChoiceInstructDataset ): # TODO: refactor as part of SuperGLUE
     def __init__( self, cache_dir: str ):
         self.metric = load_metric( 'accuracy', download_config=DownloadConfig( cache_dir=cache_dir ) )
         super().__init__( cache_dir )
-
 
     def download( self, cache_dir: str ) -> DatasetDict:
         dataset = load_dataset( 'super_glue', 'boolq', cache_dir=cache_dir )
@@ -83,8 +82,86 @@ class BoolQInstructDataset( BaseChoiceInstructDataset ):
 
         return metric
 
+
+class CopaInstructDataset( BaseChoiceInstructDataset ): # TODO: refactor as part of SuperGLUE
+    def __init__( self, cache_dir: str ):
+        self.metric = load_metric( 'accuracy', download_config=DownloadConfig( cache_dir=cache_dir ) )
+        super().__init__( cache_dir )
+
+    def download( self, cache_dir: str ) -> DatasetDict:
+        dataset = load_dataset( 'super_glue', 'copa', cache_dir=cache_dir )
+        assert isinstance( dataset, DatasetDict )
+        return dataset
+
+    @property
+    def task_type( self ) -> InstructionDatasetTask:
+        return InstructionDatasetTask.INSTRUCT_CLOSED
+
+    @property
+    def task_description( self ) -> str:
+        return ''
+
+    @property
+    def task_name( self ) -> str:
+        return 'super_glue'
+
+    @property
+    def task_subset( self ) -> str:
+        return 'copa'
+
+    def get_training_docs( self ) -> Dataset:
+        return self.dataset[ 'train' ]
+
+    def get_validation_docs( self ) -> Dataset:
+        return self.dataset[ 'validation' ]
+
+    def get_test_docs( self ) -> Dataset:
+        return self.dataset[ 'test' ]
+
+    def get_fewshot_docs( self ) -> None:
+        return None
+
+    def format_user_message( self, doc: dict ) -> Message:
+        connector = {
+            "cause": "because",
+            "effect": "therefore",
+        }[ doc["question"] ]
+
+        prompt = (
+            f"{doc['premise'].strip()[:-1]} {connector}"
+        )
+
+        return Message(
+            role='user',
+            content=prompt,
+            complete=True,
+        )
+
+    def _format_single_target( self, doc: dict ) -> Message:
+        return Message(
+            role='assistant',
+            content=[ doc['choice1'], doc[ 'choice2' ] ][ doc['label'] ],
+            complete=True,
+        )
+
+    def _get_choices( self, doc: dict ) -> list:
+        return [ 0, 1 ]
+
+    def _get_label_key( self ) -> str:
+        return 'label'
+
+    def create_unlabelled_message_target( self, doc: dict ) -> int:
+        return doc['label']
+
+    def compute_metric( self, predictions=None, references=None ) -> dict:
+        metric = self.metric.compute( predictions=predictions, references=references )
+        assert metric is not None
+
+        return metric
+
 DIRECTORY: Mapping[str, Callable[[str], BaseChoiceInstructDataset]] = {
     'boolq': BoolQInstructDataset,
+    'copa': CopaInstructDataset,
 }
 
 def main():
@@ -95,3 +172,4 @@ def main():
     cache_dir = os.environ[ 'HF_CACHE_DIR' ]
 
     BoolQInstructDataset( cache_dir )
+    CopaInstructDataset( cache_dir )
