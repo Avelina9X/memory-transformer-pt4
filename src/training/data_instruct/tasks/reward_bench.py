064332fbc19eb6d9f2fe9dde52e07bc2a4cb9f98
Avelina Kyriacou <lhk3@st-andrews.ac.uk>
2024-11-17 16:34:40 +0000

    added subset mappings and example counts

--- src/training/data_instruct/tasks/reward_bench.py
+++ src/training/data_instruct/tasks/reward_bench.py
@@ -115,3 +115,63 @@ DIRECTORY: Mapping[str, Callable[[str], BaseChoiceInstructDataset]] = {
     'hep-python': lambda cache_dir: RewardBenchInstructDataset( cache_dir=cache_dir, split='hep-python' ),
     'hep-rust': lambda cache_dir: RewardBenchInstructDataset( cache_dir=cache_dir, split='hep-rust' ),
 }
+
+EXAMPLE_COUNTS = {
+    "alpacaeval-easy": 100,
+    "alpacaeval-length": 95,
+    "alpacaeval-hard": 95,
+    "mt-bench-easy": 28,
+    "mt-bench-med": 40,
+    "mt-bench-hard": 37,
+    "math-prm": 984,  # actual length 447, upweighting to be equal to code
+    "refusals-dangerous": 100,
+    "refusals-offensive": 100,
+    "llmbar-natural": 100,
+    "llmbar-adver-neighbor": 134,
+    "llmbar-adver-GPTInst": 92,
+    "llmbar-adver-GPTOut": 47,
+    "llmbar-adver-manual": 46,
+    "xstest-should-refuse": 154,
+    "xstest-should-respond": 250,
+    "donotanswer": 136,
+    "hep-cpp": 164,
+    "hep-go": 164,
+    "hep-java": 164,
+    "hep-js": 164,
+    "hep-python": 164,
+    "hep-rust": 164,
+}
+
+SUBSET_MAPPING = {
+    "Chat": [
+        "alpacaeval-easy",
+        "alpacaeval-length",
+        "alpacaeval-hard",
+        "mt-bench-easy",
+        "mt-bench-med",
+    ],
+    "Chat Hard": [
+        "mt-bench-hard",
+        "llmbar-natural",
+        "llmbar-adver-neighbor",
+        "llmbar-adver-GPTInst",
+        "llmbar-adver-GPTOut",
+        "llmbar-adver-manual",
+    ],
+    "Safety": [
+        "refusals-dangerous",
+        "refusals-offensive",
+        "xstest-should-refuse",
+        "xstest-should-respond",
+        "donotanswer",
+    ],
+    "Reasoning": [
+        "math-prm",
+        "hep-cpp",
+        "hep-go",
+        "hep-java",
+        "hep-js",
+        "hep-python",
+        "hep-rust",
+    ],
+}
